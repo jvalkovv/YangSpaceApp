843633dd5484f30a425d1d8cdcad60033048450a
Yordan <yordan.valkov@gmail.com>
2024-12-09 21:18:19 +0200

    Remove old code

--- YangSpaceBackEnd/Data/ViewModel/PaginatedBookingsViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace YangSpaceBackEnd.Data.ViewModel
-{
-    public class PaginatedBookingsViewModel
-    {
-        public int TotalCount { get; set; }
-        public List<BookingViewModel> Bookings { get; set; } = new List<BookingViewModel>();
-
-    }
-}
