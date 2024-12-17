34b74855865d4254a60bbb0ac00864fc48baf737
Yordan <yordan.valkov@gmail.com>
2024-12-17 11:44:17 +0200

    Add responsive navbar design

--- yangspaceapp.client/src/app/shared/components/navbar/navbar.component.ts
+++ yangspaceapp.client/src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { AuthService } from '../../../auth/services/auth-service';
 import { environment } from '../../../../environments/environment';
+import { AuthService } from '../../../auth/services/auth-service';
+import { UserProfileService } from '../../../user-profile/user-profile.service';
 
 @Component({
   selector: 'app-navbar',
@@ -15,17 +16,37 @@ export class NavbarComponent implements OnInit {
   isLoggedIn: boolean = false;
   isServiceProvider: boolean = false;
   username: string | null = null;
-
-  constructor(private authService: AuthService, private router: Router) { }
+  userProfile: any = {};
+  isSidebarOpen: boolean = false;
+  constructor(private authService: AuthService, private router: Router, private userProfileService: UserProfileService) { }
 
   ngOnInit(): void {
     this.authService.isLoggedIn$.subscribe((isLoggedIn) => {
       this.isLoggedIn = isLoggedIn;
       this.username = localStorage.getItem(environment.usernameKey);
-      this.isServiceProvider = this.authService.isServiceProvider(); // Check if the user is a ServiceProvider
+      this.isServiceProvider = this.authService.isServiceProvider();
+      this.fetchUserProfile();
     });
   }
-
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+  fetchUserProfile(): void {
+    this.userProfileService.getUserProfile()
+      .subscribe({
+        next: (data) => {
+          this.userProfile = data;
+        },
+        error: (err) => {
+          console.error('Error fetching user profile:', err);
+        }
+      });
+  }
+  get profilePicture(): string {
+    return this.userProfile?.profilePictureUrl
+      ? `${environment.imageUrl}${this.userProfile.profilePictureUrl}`
+      : '';
+  }
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
